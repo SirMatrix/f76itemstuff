730f3c28d9a28547781c337d5399b35d5be477bb
SirMatrix <killer20816@gmail.com>
2022-07-17 21:56:57 +0000

    Started working on a function to gen text

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod utils;
+use egui::Vec2;
+
 use crate::utils::sheets::*;
 use crate::utils::gui::*;
 fn main() {
+  tracing_subscriber::fmt::init();
     utils::files::file_checker();
     utils::files::file_size();
     utils::sheets::make_document();
-    let native_options = eframe::NativeOptions::default();
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.initial_window_size = Some(Vec2::new(540., 960.));
     eframe::run_native("F76ItemStuff", native_options, Box::new(|cc| Box::new(MainApp::new(cc))))
 }
 
--- src/utils/gui.rs
+++ src/utils/gui.rs
@@ -1,14 +1,53 @@
 use eframe::egui;
-use egui::{TopBottomPanel, Layout, Label};
+use egui::{TopBottomPanel, Layout, Label, Visuals, Checkbox, Button, Window, Color32, CentralPanel, ScrollArea};
+use serde::{Serialize, Deserialize};
+use tracing::Subscriber;
+use super::sheets::*;
 
 
-#[derive(Default)]
+pub const PADDING: f32 = 5.0;
+const WHITE: Color32 = Color32::from_rgb(255, 255, 255);
 
-pub struct MainApp{}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AppConfig{
+     dark_mode: bool,
+
+}
+
+impl Default for AppConfig{
+    fn default() -> Self {
+        Self{dark_mode: Default::default()}
+    }
+}
+
+
+#[derive(Default, Debug)]
+
+pub struct MainApp{
+    itemnames: String
+}
+#[derive(Default, Debug)]
+struct StoredData{
+  
+
+}
 
 impl MainApp {
    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self::default()
+    }
+    fn fetch_data(&mut self ,ui: &mut eframe::egui::Ui, sheet: String){
+        let gatherstrings = read_sheet_string(sheet.to_string(), true);
+        let strings = gatherstrings.into_iter().collect();
+        let x = MainApp{ itemnames: strings };
+        for a in self.itemnames.chars(){
+            ui.add_space(PADDING);
+            let items = format!("{:?}", a);
+            ui.colored_label(WHITE, items);
+
+        }
+       
     }
     fn menu_bar(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame){
         TopBottomPanel::top("top_panel").show(ctx, |ui|{
@@ -19,12 +58,38 @@ impl MainApp {
                       if ui.button("Quit").clicked(){
                         ui.close_menu();
                       }
+                    });
+                    ui.menu_button("Load Data",|ui|{
+                        ui.menu_button("Collectibles", |ui|{
+                            if ui.button("Bobble Heads").clicked(){
+                             ScrollArea::both().show(ui, |ui|{
+                                println!("Clicked");
+                             });
+
+                            }
+                            if ui.button("Magazines").clicked(){
+                                ui.close_menu();
+                            }
+                            if ui.button("Apparel").clicked(){
+                                ui.close_menu();
+                            }
+                        });
+                        ui.menu_button("Personal", |ui|{
+                            if ui.button("For Trade").clicked(){
+                                ui.close_menu();
+                            }
+                            if ui.button("Vendor").clicked(){
+                                ui.close_menu();
+                            }
+                        })
+
                     })
                 });
             });
         });
 
     }
+   
 }
 
 impl eframe::App for MainApp {
@@ -32,6 +97,8 @@ impl eframe::App for MainApp {
         egui::CentralPanel::default().show(ctx, |ui|{
             ui.heading("Test");
             self.menu_bar(ctx, frame);
+            ctx.set_visuals(Visuals::dark());
+          
         });
     }
 
